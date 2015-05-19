401d94ca5d14e3ef01ebc2dd4ae56e5d44cabf60
HuangXing <huangxing@jabinfo.com>
2015-05-19 23:57:09 +0800

    采用coffeescript完成网站的js全部功能，但js需要根据后台接口才能使用

--- dev/coffee/page/index.coffee
+++ dev/coffee/page/index.coffee
@@ -1,13 +1,34 @@
 $(document).ready ->
   $(window).scroll ->
-    if($(this).scrollTop() > 100) 
+    if($(this).scrollTop() > 100)
       $(".go-top").css({ bottom: "110px"});
     else
       $(".go-top").css({bottom: "-110px"});
   $(".go-top").click ->
   	$("body,html").animate
   		scrollTop: 0
-  		400 
+  		400
+
+  # 拉取下下一页
+  $("#J_page").click ->
+    $(this).text("正在努力加载");
+    str="";
+    page=$(this).attr 'data';
+    $.getJSON "http://127.0.0.1:7092/blog/home/page/"+page, (data) ->
+      console.log (data);
+      if(data.length==0)
+        alert("亲,最后一页了");
+        return;
+      for i in data
+        str+="<li class='clearfix'><article>";
+        str+="<a href='/blog/home/detail/"+i.articleId+"' class='title'>"+i.title+"</a>";
+        str+='<div class="content">'+i.summary+'</div>'
+        str+='<div class="article-info"><span><i class="icon-calendar"></i>'+i.pubtime+'</span><span><i class="icon-book"></i>'+i.keyword+'</span><span><i class="icon-comments-alt"></i>'+i.reads+'</span><span><i class="icon-heart-empty"></i>'+i.ispost+'</span></div>';
+        str+="</article></li>";
+      $('.x-list').append(str);
+      page++;
+      $("#J_page").text "点击加载更多";
+      $("#J_page").attr data:''+page+'';
   # 网页加载实现动画效果
   subSomething = ->
     if(document.readyState == "loading") #当前页面加载状态
@@ -18,10 +39,6 @@ $(document).ready ->
       $("#J_spinner").hide();
   Week = ->
     week=new Date().getDay();
-    $(".cover-img").css("background-image","url(../img/"+week+".jpg)"); 
+    $(".cover-img").css("background-image","url(res/debug/img/"+week+".jpg)");
   Week();
   document.onreadystatechange = subSomething;
-
-  		
-
-  		
--- dev/debug/js/page/index.js
+++ dev/debug/js/page/index.js
@@ -16,6 +16,34 @@ $(document).ready(function() {
       scrollTop: 0
     }, 400);
   });
+  $("#J_page").click(function() {
+    var page, str;
+    $(this).text("正在努力加载");
+    str = "";
+    page = $(this).attr('data');
+    return $.getJSON("http://127.0.0.1:7092/blog/home/page/" + page, function(data) {
+      var i, _i, _len;
+      console.log(data);
+      if (data.length === 0) {
+        alert("亲,最后一页了");
+        return;
+      }
+      for (_i = 0, _len = data.length; _i < _len; _i++) {
+        i = data[_i];
+        str += "<li class='clearfix'><article>";
+        str += "<a href='/blog/home/detail/" + i.articleId + "' class='title'>" + i.title + "</a>";
+        str += '<div class="content">' + i.summary + '</div>';
+        str += '<div class="article-info"><span><i class="icon-calendar"></i>' + i.pubtime + '</span><span><i class="icon-book"></i>' + i.keyword + '</span><span><i class="icon-comments-alt"></i>' + i.reads + '</span><span><i class="icon-heart-empty"></i>' + i.ispost + '</span></div>';
+        str += "</article></li>";
+      }
+      $('.x-list').append(str);
+      page++;
+      $("#J_page").text("点击加载更多");
+      return $("#J_page").attr({
+        data: '' + page + ''
+      });
+    });
+  });
   subSomething = function() {
     if (document.readyState === "loading") {
       return $("#J_spinner").show();
@@ -28,7 +56,7 @@ $(document).ready(function() {
   Week = function() {
     var week;
     week = new Date().getDay();
-    return $(".cover-img").css("background-image", "url(../img/" + week + ".jpg)");
+    return $(".cover-img").css("background-image", "url(res/debug/img/" + week + ".jpg)");
   };
   Week();
   return document.onreadystatechange = subSomething;
